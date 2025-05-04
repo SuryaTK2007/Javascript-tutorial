d588e478d72166bcc07a9947e79a910d1fe0fb3f
Surya TK <suryatk2007@gmail.com>
2025-05-04 16:23:43 +0530

    Tried on my own

--- Prime_numer/script.js
+++ Prime_numer/script.js
@@ -1,31 +1,29 @@
-function isPrime(n) {
-    if (n < 2) return false;
-    if (n === 2) return true;
-    if (n % 2 === 0) return false;
-    const limit = Math.sqrt(n);
-    for (let i = 3; i <= limit; i += 2) {
-      if (n % i === 0) return false;
+function isPrime(n){
+    if(n<2) return false;
+    if(n==2) return true;
+    if(n%2==0) return false;
+    let limit=Math.sqrt(n);
+    for(let i=3; i<=limit; i++){
+        if(n%i==0) return false;
     }
     return true;
-  }
-  document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('numInput');
-    const result = document.getElementById('result');
-    const button = document.getElementById('checkBtn');
-  
-    button.addEventListener('click', () => {
-      const value = parseInt(input.value, 10);
-  
-      if (isNaN(value)) {
-        result.textContent = 'Please enter a valid number.';
-        return;
-      }
-  
-      if (isPrime(value)) {
-        result.textContent = `${value} is a prime number `;
-      } else {
-        result.textContent = `${value} is not a prime number`;
-      }
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    const input=document.getElementById('numInput');
+    const check=document.getElementById('checkBtn');
+    const result=document.getElementById('result');
+    check.addEventListener('click',()=>{
+        const value=parseInt(input.value, 10);
+        if(isNaN(value)){
+            result.textContent="Enter a number";
+            return;
+        }
+        if(isPrime(value)){
+            result.textContent="It is a prime number";
+            return;
+        }
+        else{
+            result.textContent="It is not a prime number";
+        }
     });
-  });
-  
+});
